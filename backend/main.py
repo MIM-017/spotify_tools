f0f918949d101bac109d862c1f6bda814dcba1dc
Artem Maksimov <artem20150412@gmail.com>
2025-12-08 16:38:35 -0800

    Added CORS middleware. Added the stub for retrieve_playlists endpoint.

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from backend.services.playlist_cleaner import PlaylistCleaner
 
+origins = ["http://127.0.0.1:5500"]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
 playlist_cleaner = PlaylistCleaner()
 @app.get("/playlist_formatted/{playlist_id}")
 async def get_playlist_kuci_formatted(playlist_id: str):
@@ -14,3 +24,6 @@ async def get_playlist_kuci_formatted(playlist_id: str):
 async def refine_playlist(playlist_id: str):
     playlist_cleaner.refine_playlist(playlist_id=playlist_id)
 
+@app.get("/retrieve_playlists/")
+async def retrieve_playlists():
+    pass
