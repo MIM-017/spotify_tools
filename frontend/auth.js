d3b0db2c740a8b956781f64b4f1e37eb6e2a3132
Artem Maksimov <artem20150412@gmail.com>
2025-12-14 12:33:46 -0800

    Removed the link to dashboard from the home page. Implemented the authorization flow with silent redirect to dashboard

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,6 +1,7 @@
 import { API_ROOT } from "./config.js";
 
-const spotifyAuthorizeURL = `${API_ROOT}/authorize_spotify/`;
+const spotifyAuthorizeURL = `${API_ROOT}/authorize_spotify`;
+const tokenCheckURL = `${API_ROOT}/check_tokens`;
 let _accessToken = null;
 
 export async function fetchAccessToken() {
@@ -20,7 +21,19 @@ export function getAccessToken() {
     return _accessToken;
 }
 
+async function checkTokens() {
+    const result = await fetch(tokenCheckURL, {"credentials": "include"});
+    console.log(result.status);
+    console.log("Test");
+    if (result.status !== 200) {
+        window.location.href = spotifyAuthorizeURL;
+    }
+    else if(result.status === 200) {
+        window.location.href = "/dashboard.html";
+    }
+}
+
 const loginButton = document.getElementById("loginButton");
 if (loginButton) {
-    loginButton.addEventListener("click", () => {window.location.href = spotifyAuthorizeURL;});
+    loginButton.addEventListener("click", async () => await checkTokens());
 }
