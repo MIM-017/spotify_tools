afb6e82b81d754da6a8d34335223537793d619c3
Artem Maksimov <artem20150412@gmail.com>
2025-12-29 20:09:01 -0800

    Included the security router. Reformatted the file. Modified the retrieve_playlists endpoint function to account for a generator being returned

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
 from backend.schemas import Playlist
+from backend.security import *
 from backend.services.playlist_cleaner import PlaylistCleaner
 
 origins = ["http://127.0.0.1:5500"]
@@ -16,26 +17,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(auth_router)
+
 playlist_cleaner = PlaylistCleaner()
+
+
 @app.get("/playlist_formatted/{playlist_id}")
 async def get_playlist_kuci_formatted(playlist_id: str):
     result = playlist_cleaner.get_playlist_kuci_formatted(playlist_id=playlist_id)
     return list(result)
 
+
 @app.get("/refine_playlist/{playlist_id}")
 async def refine_playlist(playlist_id: str):
     playlist_cleaner.refine_playlist(playlist_id=playlist_id)
 
-@app.get("/retrieve_playlists/")
+
+@app.get("/retrieve_playlists")
 async def retrieve_playlists(check_for_a_play: bool | None = False) -> list[Playlist]:
-    return list(playlist_cleaner.get_marked_playlists(check_for_a_play=check_for_a_play))  # TODO: Consider converting to a stream for better performance
+    return list(playlist_cleaner.get_marked_playlists(
+        check_for_a_play=check_for_a_play))  # TODO: Consider converting to a stream for better performance
+
 
 @app.get("/authorize_spotify")
 async def authorize_spotify():
     return RedirectResponse(f"{playlist_cleaner.auth_manager.get_authorize_url()}")
-
-@app.get("/authorize_user")
-async def authorize_user(code: str):
-    access_token = playlist_cleaner.auth_manager.get_access_token(code)
-    username = playlist_cleaner.get_spotify_user_username(access_token)
-    playlist_cleaner.save_token_to_cache(username, access_token)
