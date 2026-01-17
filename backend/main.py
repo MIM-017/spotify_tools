e6e5bda47af03bd4c13e0826328b51b747fe765b
Artem Maksimov <artem20150412@gmail.com>
2026-01-16 17:25:49 -0800

    Changed all the endpoints to regular def (synchronous). Added auth check to refine_playlist endpoint

--- backend/main.py
+++ backend/main.py
@@ -22,24 +22,25 @@ playlist_cleaner = PlaylistCleaner()
 
 
 @app.get("/playlist_formatted/{playlist_id}")
-async def get_playlist_kuci_formatted(playlist_id: str):
+def get_playlist_kuci_formatted(playlist_id: str):
     result = playlist_cleaner.get_playlist_kuci_formatted(playlist_id=playlist_id)
     return list(result)
 
 
 @app.get("/refine_playlist/{playlist_id}")
-async def refine_playlist(playlist_id: str):
+def refine_playlist(user_id: Annotated[str, Depends(get_user_id)], playlist_id: str):
+    playlist_cleaner.set_current_username(user_id)
     playlist_cleaner.refine_playlist(playlist_id=playlist_id)
 
 
 @app.get("/retrieve_playlists")
-async def retrieve_playlists(user_id: Annotated[str, Depends(get_user_id)], check_for_a_play: bool | None = False):
+def retrieve_playlists(user_id: Annotated[str, Depends(get_user_id)], check_for_a_play: bool | None = False):
     playlist_cleaner.set_current_username(user_id)
     return StreamingResponse((i.model_dump_json() + "\n" for i in playlist_cleaner.get_marked_playlists(check_for_a_play=check_for_a_play)),
                              media_type="application/x-ndjson")
 
 @app.get("/retrieve_tracks/{playlist_id}")
-async def retrieve_tracks(playlist_id: str,
+def retrieve_tracks(playlist_id: str,
                           user_id: Annotated[str, Depends(get_user_id)],
                           check_for_a_play: bool | None = False):
     playlist_cleaner.set_current_username(user_id)
@@ -48,5 +49,5 @@ async def retrieve_tracks(playlist_id: str,
                              media_type="application/x-ndjson")
 
 @app.get("/authorize_spotify")
-async def authorize_spotify():
+def authorize_spotify():
     return RedirectResponse(f"{playlist_cleaner.auth_manager.get_authorize_url()}")
